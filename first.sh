a198d9acf206e7a48dab13fe7506f21f28023d46
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2024-01-17 13:09:23 +0000

    update

--- first.sh
+++ first.sh
@@ -1,5 +1,5 @@
 sudo apt update
 #sudo apt install git build-essential
 sudo apt install openjdk-11-jre-headless
-cd ./server
+cd /workspaces/MCshare/server
 java -Xmx6144M -Xms1024M -jar server.jar nogui
